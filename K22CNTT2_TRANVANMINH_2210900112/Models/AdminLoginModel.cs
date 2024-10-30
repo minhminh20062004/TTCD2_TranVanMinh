f84fe86c10fab2f5e4f28d0c713fc52c8f43b289
minhminh20062004 <tranvanminh20062004@gmail.com>
2024-10-30 23:28:50 +0700

    project2buoi1

---
+++ K22CNTT2_TRANVANMINH_2210900112/Models/AdminLoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace K22CNTT2_TRANVANMINH_2210900112.Models
+{
+    public class AdminLoginModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
